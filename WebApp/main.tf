a1b99208ec2612eb96d1333cf4a087b77992fe56
Chithra476 <chithraragavan31896@gmail.com>
2024-08-07 19:42:42 +0530

    Update main.tf

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -1,113 +1,37 @@
 terraform {
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "= 2.99"
     }
   }
 }
 
 provider "azurerm" {
-  skip_provider_registration = "true"
   features {}
+
+  skip_provider_registration = true
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "TFNet" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
+resource "azurerm_app_service_plan" "svcplan" {
+  name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  tags = {
-    environment = "Terraform VNET"
+  sku {
+    tier = var.app_service_plan_tier
+    size = var.app_service_plan_size
   }
 }
 
-# Create subnet
-resource "azurerm_subnet" "tfsubnet" {
-  name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.TFNet.name
-  address_prefixes     = var.subnet_address_prefixes
-}
-
-# Deploy Public IP
-resource "azurerm_public_ip" "example" {
-  name                = var.public_ip_name
+resource "azurerm_app_service" "appsvc" {
+  name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = var.public_ip_allocation_method
-  sku                 = var.public_ip_sku
-}
+  app_service_plan_id = azurerm_app_service_plan.svcplan.id
 
-# Create NIC
-resource "azurerm_network_interface" "example" {
-  name                = var.nic_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = var.ip_config_name
-    subnet_id                     = azurerm_subnet.tfsubnet.id
-    private_ip_address_allocation = var.private_ip_allocation_method
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-# Create Boot Diagnostic Account
-resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.storage_account_replication_type
-
-  tags = {
-    environment = "Boot Diagnostic Storage"
-    CreatedBy   = "Admin"
-  }
-}
-
-# Create Virtual Machine
-resource "azurerm_virtual_machine" "example" {
-  name                  = var.vm_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = var.vm_size
-  delete_os_disk_on_termination = var.delete_os_disk_on_termination
-  delete_data_disks_on_termination = var.delete_data_disks_on_termination
-
-  storage_image_reference {
-    publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
-  }
-
-  storage_os_disk {
-    name              = var.os_disk_name
-    disk_size_gb      = var.os_disk_size_gb
-    caching           = var.os_disk_caching
-    create_option     = var.os_disk_create_option
-    managed_disk_type = var.os_disk_managed_disk_type
-  }
-
-  os_profile {
-    computer_name  = var.computer_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = var.disable_password_authentication
-  }
-
-  boot_diagnostics {
-    enabled     = var.boot_diagnostics_enabled
-    storage_uri = azurerm_storage_account.sa.primary_blob_endpoint
+  site_config {
+    dotnet_framework_version = var.dotnet_framework_version
+    scm_type                 = var.scm_type
   }
 }
